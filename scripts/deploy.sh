aee9ad91249561ecfda62ea8678ae396bdf31d94
Matt Harrison <matthew.harrison@homes.com>
2021-12-13 10:33:14 -0500

    updated ftp migration script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,4 @@
-rm -rf ../rbc/projects/longbox/
+rm -rf ../rbc/apps/longbox/
 cp -r build/ longbox/
-mv longbox ../rbc/projects/longbox
-scp -r ../rbc/projects/longbox kittenb1@rootbeercomics.com:/home1/kittenb1/www/projects/
+mv longbox ../rbc/apps/longbox
+scp -r ../rbc/apps/longbox kittenb1@rootbeercomics.com:/home1/kittenb1/www/apps/
